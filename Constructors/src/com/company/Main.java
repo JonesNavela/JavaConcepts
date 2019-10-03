b965ae40bd430d4a1bba77c486bbfeacc0408593
JonesNavela <navelajones@gmail.com>
2019-10-03 13:01:26 +0200

    Test methods from Vip and Account class

---
+++ Constructors/src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    BankAccount travelingSavings = new BankAccount("12345", 9000000, "Jones Navela", "NavelaJones@gmail.com", "074 521 5647");
+        System.out.println(travelingSavings.getCustomerName());
+        System.out.println(travelingSavings.getCustomerEmail());
+        System.out.println(travelingSavings.getCustomerPhoneNumber());
+        System.out.println(travelingSavings.getNumber());
+        System.out.println(travelingSavings.getBalance());
+
+        System.out.println("***********************");
+
+	    travelingSavings.withdrawal(200);
+
+	    travelingSavings.deposit(50);
+	    travelingSavings.withdrawal(200);
+
+        System.out.println("***********************");
+
+        BankAccount myAccount = new BankAccount("213448", 500000, "Rhulani Navela","rhunavela@gmail.com","0723456954");
+        System.out.println("Account name: " + myAccount.getCustomerName());
+        System.out.println("Account email: " + myAccount.getCustomerEmail());
+        System.out.println("Account number: " + myAccount.getNumber());
+        System.out.println("Account balance: " + myAccount.getBalance());
+
+        System.out.println("*********************");
+
+        VipCustomer goldenAccount = new VipCustomer();
+
+        goldenAccount.getName();
+    }
+}
+
+//create a new class for a bank account
+//create fields for the account number, balance, customer name, email and phone number
+//
+//create getters and setters for each field ( So, a setter is a method that updates value of a variable. And a getter is a method that reads value of a variable.)
+//create two additional methods
+//1. To allow the customer to deposit funds (this should increment the balance field)
+//2. To allow the customer to withdraw funds. This should deduct from the balance field,
+//but not allow the withdrawal to complete if there are insuffucuent funds
+//You will want to create various code in the Main class (the one created by IntelliJ) to
+//confirm your code is working
+//add some System.out.println's in the two methods above as well.
