ae00e663fb72811fd92bd68cef510b89fad24a7a
JonesNavela <navelajones@gmail.com>
2019-10-03 13:17:36 +0200

    Edit  first and second constructors correctly

--- Constructors/src/com/company/VipCustomer.java
+++ Constructors/src/com/company/VipCustomer.java
@@ -7,15 +7,17 @@ public class VipCustomer {
     private String email;
 
     public VipCustomer() {
-        this("Default name", 2.50,"Default email");
+        this("Default name", 50000,"Default@email.com");
         System.out.println("Empty constructor called");
     }
 
-    public VipCustomer(String name, String email) {
-        this("rhulani", 2.50, "rhu@gmail.com");
+    public VipCustomer(String name, double creditLimit) {
+        this(name, creditLimit, "Unknown@email.com");
+
     }
 
     public VipCustomer(String name, double creditLimit, String email) {
+        System.out.println("Constructor with parameters called again");
         this.name = name;
         this.creditLimit = creditLimit;
         this.email = email;
