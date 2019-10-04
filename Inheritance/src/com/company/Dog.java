5e7a147c0ddb21ae1885fbbe2cde16c5b82688c8
JonesNavela <navelajones@gmail.com>
2019-10-04 13:41:59 +0200

    we know all dogs have a body and brain from the Animal superclass so we won't force the programmer to define those properties again in the dog class

--- Inheritance/src/com/company/Dog.java
+++ Inheritance/src/com/company/Dog.java
@@ -1,14 +1,14 @@
 package com.company;
 
 public class Dog extends Animal {
-
+    // Unique characteristics related to a dog specifically on top of the base characteristics of an animal
     private int eyes;
     private int legs;
     private int tail;
     private int teeth;
     private String coat;
 
-    public Dog(String name, int brain, int body, int size, int weight) {
-        super(name, brain, body, size, weight);
+    public Dog(String name, int size, int weight) {
+        super(name, 1, 1, size, weight);
     }
 }
