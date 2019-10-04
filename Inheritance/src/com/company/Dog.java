740e9b60cd51f1976b7e67bd12df1231e92f963a
JonesNavela <navelajones@gmail.com>
2019-10-04 13:43:37 +0200

    Add fields as extra parameters

--- Inheritance/src/com/company/Dog.java
+++ Inheritance/src/com/company/Dog.java
@@ -8,7 +8,7 @@ public class Dog extends Animal {
     private int teeth;
     private String coat;
 
-    public Dog(String name, int size, int weight) {
+    public Dog(String name, int size, int weight, int eyes, int legs, int tail, int teeth, String coat) {
         super(name, 1, 1, size, weight);
     }
 }
