6ce35c0b8f5fc6e3488fbe3dd97cdd0f4536381e
JonesNavela <navelajones@gmail.com>
2019-10-04 13:45:52 +0200

    Initialize new fields

--- Inheritance/src/com/company/Dog.java
+++ Inheritance/src/com/company/Dog.java
@@ -10,5 +10,10 @@ public class Dog extends Animal {
 
     public Dog(String name, int size, int weight, int eyes, int legs, int tail, int teeth, String coat) {
         super(name, 1, 1, size, weight);
+        this.eyes = eyes;
+        this.legs = legs;
+        this.tail = tail;
+        this.teeth = teeth;
+        this.coat = coat;
     }
 }
