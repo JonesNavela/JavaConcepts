4dce116df59c926bc620488e2e713a4b78a24e3d
JonesNavela <navelajones@gmail.com>
2019-10-04 13:14:44 +0200

    Define inheritance and add new Animal class

--- Inheritance/src/com/company/Main.java
+++ Inheritance/src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // Inheritance in Java is a mechanism in which one object acquires all the properties and behaviors
+        // of a parent object. It is an important part of OOPs (Object Oriented programming system).
+        // The idea behind inheritance in Java is that you can create new classes
+        // that are built upon existing classes.
     }
 }
