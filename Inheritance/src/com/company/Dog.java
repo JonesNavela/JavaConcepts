30f954566b485d750ad002f2785958b7cc5169d3
JonesNavela <navelajones@gmail.com>
2019-11-28 14:18:24 +0200

    override eat method from animal class in the dog class for chew method

--- Inheritance/src/com/company/Dog.java
+++ Inheritance/src/com/company/Dog.java
@@ -17,4 +17,14 @@ public class Dog extends Animal {
         this.teeth = teeth;
         this.coat = coat;
     }
+
+    private void chew() {
+        System.out.println("Dog.chew() called");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Dog.eat() called");
+        super.eat();
+    }
 }
--- Inheritance/src/com/company/Main.java
+++ Inheritance/src/com/company/Main.java
@@ -7,5 +7,10 @@ public class Main {
         // of a parent object. It is an important part of OOPs (Object Oriented programming system).
         // The idea behind inheritance in Java is that you can create new classes
         // that are built upon existing classes.
+
+        Animal animal = new Animal("Animal", 1, 1, 5,5);
+
+        Dog dog = new Dog("Rusty",10,8,2, 4, 1, 24, "fur");
+        System.out.println("My dog's name is : " + dog.getName() + " and he weighs " + dog.getWeight() + "KG ");
     }
 }
