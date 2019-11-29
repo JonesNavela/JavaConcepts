c1c8e1ab13794031ac911b7e97980e071184d0e2
JonesNavela <navelajones@gmail.com>
2019-11-29 12:52:45 +0200

    add constructor

--- Inheritance/src/com/company/Fish.java
+++ Inheritance/src/com/company/Fish.java
@@ -1,8 +1,12 @@
 package com.company;
 
-public class Fish {
+public class Fish extends Animal {
 
     private int gills;
     private int eyes;
     private int fins;
+
+    public Fish(String name, int size, int weight, int gills, int eyes, int fins) {
+        super(name, 1, 1, size, weight);
+    }
 }
