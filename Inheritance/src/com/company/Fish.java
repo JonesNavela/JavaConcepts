6f88d8922e42eb826a0cfd665076cac0e1075403
JonesNavela <navelajones@gmail.com>
2019-11-29 12:58:16 +0200

    add methods unique to fish and model swim events

--- Inheritance/src/com/company/Fish.java
+++ Inheritance/src/com/company/Fish.java
@@ -9,4 +9,22 @@ public class Fish extends Animal {
     public Fish(String name, int size, int weight, int gills, int eyes, int fins) {
         super(name, 1, 1, size, weight);
     }
+
+    private void rest() {
+
+    }
+
+    private void moveMuscles() {
+
+    }
+
+    private void moveBackFin() {
+
+    }
+
+    private void swim(int speed) {
+        moveMuscles();
+        moveBackFin();
+        super.move(speed);
+    }
 }
